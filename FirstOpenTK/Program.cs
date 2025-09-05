f4343d5e2989fc401737a664eee4c1f3e3959cde
ZhouJ <164371491+Zylz8@users.noreply.github.com>
2025-09-05 11:55:46 -0400

    Add files via upload

---
+++ FirstOpenTK/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FirstOpenTK;
+//using FirstOpenTk;
+
+
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace FirstOpenTK {
+    // Main Entry Point for C# Console
+    class Program {
+        static void Main(String[] args) {
+            using (Game game = new Game()) {
+                game.Run();
+            }
+        }
+    }
+}
