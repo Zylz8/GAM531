d22e588053fe21839591c452a5a14d959bd9e8b1
ZhouJ <164371491+Zylz8@users.noreply.github.com>
2025-09-19 14:45:23 -0400

    Add files via upload

---
+++ Exercise_W3/Part2/Week3Rendering/Game.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+
+namespace WindowEngine
+{
+    public class Game : GameWindow
+    {
+        private int vertexBufferHandle;
+        private int shaderProgramHandle;
+        private int vertexArrayHandle;
+        private int elementBufferHandle;
+
+        private int modelLoc, viewLoc, projLoc;
+
+        // Arrays to store transformation state for each triangle
+        private float[] rotationAngles;
+        private float[] scaleFactors;
+        private bool[] scalingUp;
+
+        // Number of triangles
+        private const int TriangleCount = 1;
+
+        public Game()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.Size = new Vector2i(1280, 768);
+            this.CenterWindow(this.Size);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(new Color4(0.5f, 0.7f, 0.8f, 1f));
+
+            // Define a single triangle in normalized device coordinates
+            float[] vertices = new float[]
+            {
+                // positions        // colors
+                -0.5f,  0.5f, 0f,   1f, 0f, 0f, // top-left, red
+                -0.5f, -0.5f, 0f,   0f, 1f, 0f, // bottom-left, green
+                0.5f, -0.5f, 0f,   0f, 0f, 1f, // bottom-right, blue
+                0.5f,  0.5f, 0f,   1f, 1f, 0f  // top-right, yellow
+            };
+
+            uint[] indices = new uint[]
+            {
+                0, 1, 2, // first triangle (top-left, bottom-left, bottom-right)
+                0, 2, 3  // second triangle (top-left, bottom-right, top-right)
+            };
+
+
+
+
+            // Generate VBO
+            vertexBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            // Generate EBO
+            elementBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, elementBufferHandle);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            // Generate VAO
+            vertexArrayHandle = GL.GenVertexArray();
+            GL.BindVertexArray(vertexArrayHandle);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, elementBufferHandle);
+
+            // Position attribute
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // Color attribute
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            GL.BindVertexArray(0);
+
+
+            // Vertex shader with model, view, projection matrices
+            string vertexShaderCode = @"
+                #version 330 core
+                layout(location = 0) in vec3 aPosition;
+                layout(location = 1) in vec3 aColor;
+
+                out vec3 vColor;
+
+                uniform mat4 uModel;
+                uniform mat4 uView;
+                uniform mat4 uProj;
+
+                void main()
+                {
+                    vColor = aColor;  // Pass color to fragment shader
+
+                    // Apply model (scaling, rotation), view, and projection transformations to the vertex
+                    gl_Position = uProj * uView * uModel * vec4(aPosition, 1.0);
+                }
+            ";
+
+            string fragmentShaderCode = @"
+                #version 330 core
+                in vec3 vColor;
+                out vec4 FragColor;
+
+                void main()
+                {
+                    FragColor = vec4(vColor, 1.0);
+                }
+
+            ";
+
+            // Shaders
+            int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderHandle, vertexShaderCode);
+            GL.CompileShader(vertexShaderHandle);
+            CheckShaderCompile(vertexShaderHandle, "Vertex Shader");
+
+            int fragmentShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShaderHandle, fragmentShaderCode);
+            GL.CompileShader(fragmentShaderHandle);
+            CheckShaderCompile(fragmentShaderHandle, "Fragment Shader");
+
+            shaderProgramHandle = GL.CreateProgram();
+            GL.AttachShader(shaderProgramHandle, vertexShaderHandle);
+            GL.AttachShader(shaderProgramHandle, fragmentShaderHandle);
+            GL.LinkProgram(shaderProgramHandle);
+
+            GL.DetachShader(shaderProgramHandle, vertexShaderHandle);
+            GL.DetachShader(shaderProgramHandle, fragmentShaderHandle);
+            GL.DeleteShader(vertexShaderHandle);
+            GL.DeleteShader(fragmentShaderHandle);
+
+            // Get uniform locations
+            modelLoc = GL.GetUniformLocation(shaderProgramHandle, "uModel");
+            viewLoc = GL.GetUniformLocation(shaderProgramHandle, "uView");
+            projLoc = GL.GetUniformLocation(shaderProgramHandle, "uProj");
+
+            // Initialize transformation arrays for multiple triangles
+            rotationAngles = new float[TriangleCount];
+            scaleFactors = new float[TriangleCount];
+            scalingUp = new bool[TriangleCount];
+
+            for (int i = 0; i < TriangleCount; i++)
+            {
+                rotationAngles[i] = i * 0.5f; // stagger initial rotations
+                scaleFactors[i] = 1f;
+                scalingUp[i] = true;
+            }
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            // Update rotation and scale for each triangle independently
+            for (int i = 0; i < TriangleCount; i++)
+            {
+                // Rotate continuously
+                rotationAngles[i] += (float)args.Time * (i + 1); // different speed for each triangle
+
+                // Oscillating scale between 0.5 and 1.5
+                if (scalingUp[i])
+                {
+                    scaleFactors[i] += (float)args.Time;
+                    if (scaleFactors[i] >= 1.5f) scalingUp[i] = false;
+                }
+                else
+                {
+                    scaleFactors[i] -= (float)args.Time;
+                    if (scaleFactors[i] <= 0.5f) scalingUp[i] = true;
+                }
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.UseProgram(shaderProgramHandle);
+
+            // View matrix (camera)
+            Matrix4 view = Matrix4.LookAt(new Vector3(0, 0, 5), Vector3.Zero, Vector3.UnitY);
+            GL.UniformMatrix4(viewLoc, false, ref view);
+
+            // Projection matrix (perspective)
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(
+                MathHelper.DegreesToRadians(60f),
+                (float)Size.X / Size.Y,
+                0.1f,
+                100f
+            );
+            GL.UniformMatrix4(projLoc, false, ref projection);
+
+            // Model matrix (scale + rotation)
+            Matrix4 model = Matrix4.CreateScale(1f) *
+                            Matrix4.CreateRotationY(0f);
+            GL.UniformMatrix4(modelLoc, false, ref model);
+
+            // Draw square using EBO
+            GL.BindVertexArray(vertexArrayHandle);
+            GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+            GL.BindVertexArray(0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(vertexBufferHandle);
+
+            GL.BindVertexArray(0);
+            GL.DeleteVertexArray(vertexArrayHandle);
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(shaderProgramHandle);
+
+            base.OnUnload();
+        }
+
+        private void CheckShaderCompile(int shaderHandle, string shaderName)
+        {
+            GL.GetShader(shaderHandle, ShaderParameter.CompileStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(shaderHandle);
+                Console.WriteLine($"Error compiling {shaderName}: {infoLog}");
+            }
+        }
+    }
+}
