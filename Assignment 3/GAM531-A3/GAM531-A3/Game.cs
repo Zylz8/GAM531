dd9decf961e31d5dddae5e4078facbc24d6c9c48
ZhouJ <164371491+Zylz8@users.noreply.github.com>
2025-09-22 14:22:38 -0400

    Add files via upload

--- Assignment 3/GAM531-A3/GAM531-A3/Game.cs
+++ Assignment 3/GAM531-A3/GAM531-A3/Game.cs
@@ -47,12 +47,12 @@ namespace WindowEngine
          */
         private uint[] indices =
         {
-            0, 1, 2, 2, 3, 0, // Back
-            4, 5, 6, 6, 7, 4, // Front
-            0, 4, 7, 7, 3, 0, // Left
-            1, 5, 6, 6, 2, 1, // Right
-            3, 2, 6, 6, 7, 3, // Top
-            0, 1, 5, 5, 4, 0  // Bottom
+            0, 1, 2, 2, 3, 0, // Back Square
+            4, 5, 6, 6, 7, 4, // Front Square
+            0, 4, 7, 7, 3, 0, // Left Side Square
+            1, 5, 6, 6, 2, 1, // Right Side Sqaure
+            3, 2, 6, 6, 7, 3, // Top Sqaure
+            0, 1, 5, 5, 4, 0  // Bottom Square
         };
 
         public Game()
